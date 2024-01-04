99bc231725438c0a6937959c1f5cb667086ea90b
sam <idoru42@gmail.com>
2024-01-04 05:59:58 +0000

    train.sh, for a bit more interim reproducibility. soon we should look at our own script

---
+++ train.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+LR="2e-4"
+
+MAX_SEQ_LEN=512
+
+MODEL_NAME="mistralai/Mistral-7B-v.01"
+MODEL_NAME="OpenPipe/mistral-ft-optimized-1218"
+
+DATASET_NAME="timdettmers/openassistant-guanaco"
+DATASET_TEXT_FIELD="text"
+
+BATCH_SIZE=1
+GRADIENT_ACCUMULATION_STEPS=16
+GRADIENT_CHECKPOINTING=false
+
+TRAIN_EPOCHS=25
+
+OUTPUT_DIR="output"
+
+python examples/scripts/sft.py --model_name "$MODEL_NAME" \
+  --dataset_name "$DATASET_NAME" \
+  --dataset_text_field "$DATASET_TEXT_FIELD" \
+  --report_to wandb \
+  --learning_rate "$LR" \
+  --seq_length "$MAX_SEQ_LEN" \
+  --use_peft \
+  --output_dir "$OUTPUT_DIR" \
+  --gradient_checkpointing "$GRADIENT_CHECKPOINTING" \
+  --batch_size "$BATCH_SIZE" \
+  --peft_lora_r 64 \
+  --num_train_epochs "$TRAIN_EPOCHS"
